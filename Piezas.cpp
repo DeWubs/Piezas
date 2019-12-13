d9807e49b94d622e4dba2f7bf01801b171969b24
DeWubs <rwprouty55@gmail.com>
2019-12-13 14:32:25 -0800

    Implemented drop peice, peice at row, and some tests for drop piece

--- Piezas.cpp
+++ Piezas.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 /** CLASS Piezas
  * Class for representing a Piezas vertical board, which is roughly based
- * on the game "Connect Four" where pieces are placed in a column and 
+ * on the game "Connect Four" where pieces are placed in a column and
  * fall to the bottom of the column, or on top of other pieces already in
  * that column. For an illustration of the board, see:
  *  https://en.wikipedia.org/wiki/Connect_Four
@@ -17,11 +17,17 @@
 
 
 /**
- * Constructor sets an empty board (default 3 rows, 4 columns) and 
+ * Constructor sets an empty board (default 3 rows, 4 columns) and
  * specifies it is X's turn first
 **/
 Piezas::Piezas()
 {
+  turn = X;
+  for(i = 0; i < BOARD_ROWS; i++){
+    for(j= 0; j < BOARD_COLS; j++){
+      Board[i][j] = Blank;
+    }
+  }
 }
 
 /**
@@ -30,19 +36,50 @@ Piezas::Piezas()
 **/
 void Piezas::reset()
 {
+  for(i = 0; i < BOARD_ROWS; i++){
+    for(j= 0; j < BOARD_COLS; j++){
+      Board[i][j] = Blank;
+    }
+  }
 }
 
 /**
  * Places a piece of the current turn on the board, returns what
- * piece is placed, and toggles which Piece's turn it is. dropPiece does 
+ * piece is placed, and toggles which Piece's turn it is. dropPiece does
  * NOT allow to place a piece in a location where a column is full.
- * In that case, placePiece returns Piece Blank value 
+ * In that case, placePiece returns Piece Blank value
  * Out of bounds coordinates return the Piece Invalid value
  * Trying to drop a piece where it cannot be placed loses the player's turn
-**/ 
+**/
 Piece Piezas::dropPiece(int column)
 {
-    return Blank;
+  if(column > (BOARD_COLS - 1) ){
+      if(turn == X){
+        turn = O;
+        return Invalid;
+      }
+    else{
+        turn = X;
+        return Invalid;
+      }
+    }
+    if(Board[0][column] != ' '){
+      return Blank;
+    }
+    for(int i = 2; i >=0; i--){
+      if(Board[i][column] == ' '){
+        if(turn == X){
+          Board[i][column] = X;
+          turn = O;
+          return X;
+        }
+        else{
+          Board[i][column] = O;
+          turn = X;
+          return O;
+        }
+      }
+    }
 }
 
 /**
@@ -51,13 +88,19 @@ Piece Piezas::dropPiece(int column)
 **/
 Piece Piezas::pieceAt(int row, int column)
 {
+  if(row > (BOARD_ROWS - 1) || column > (BOARD_COLS - 1) ){
+    return Invalid;
+  }
+  if(Board[row][column] == ' '){
     return Blank;
+  }
+  return Board[row][column];
 }
 
 /**
  * Returns which Piece has won, if there is a winner, Invalid if the game
  * is not over, or Blank if the board is filled and no one has won ("tie").
- * For a game to be over, all locations on the board must be filled with X's 
+ * For a game to be over, all locations on the board must be filled with X's
  * and O's (i.e. no remaining Blank spaces). The winner is which player has
  * the most adjacent pieces in a single line. Lines can go either vertically
  * or horizontally. If both X's and O's have the same max number of pieces in a
--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -4,17 +4,45 @@
 
 #include <gtest/gtest.h>
 #include "Piezas.h"
- 
+
 class PiezasTest : public ::testing::Test
 {
 	protected:
 		PiezasTest(){} //constructor runs before each test
 		virtual ~PiezasTest(){} //destructor cleans up after tests
 		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor) 
+		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
 TEST(PiezasTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}
+/*Tests for drop peice*/
+TEST(PiezasTest, dropPieceReturnX)
+{
+  Piezas board;
+	ASSERT_EQ(board.dropPiece(0), X);
+}
+
+TEST(PiezasTest, dropPieceReturnY)
+{
+  Piezas board;
+  board.dropPiece(0);
+	ASSERT_EQ(board.dropPiece(0), Y);
+}
+
+TEST(PiezasTest, dropPieceReturnInval)
+{
+  Piezas board;
+	ASSERT_EQ(board.dropPiece(4), X);
+}
+
+TEST(PiezasTest, dropPieceReturnBlank)
+{
+  Piezas board;
+  board.dropPiece(0);
+  board.dropPiece(0);
+  board.dropPiece(0);
+	ASSERT_EQ(board.dropPiece(0), Blank);
 }
