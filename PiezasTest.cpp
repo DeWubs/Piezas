5ca8e7ca00ebff32966ec8b53c42d6330a7615bf
DeWubs <rwprouty55@gmail.com>
2019-12-13 17:58:49 -0800

    added test for xwin and o win

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -106,3 +106,39 @@ TEST(PiezasTest, tieGame)
   board.dropPiece(3);
 	ASSERT_EQ(board.gameState(), Invalid);
 }
+
+TEST(PiezasTest, xWin)
+{
+  Piezas board;
+  board.dropPiece(0);
+  board.dropPiece(0);
+  board.dropPiece(1);
+  board.dropPiece(1);
+  board.dropPiece(2);
+  board.dropPiece(0);
+  board.dropPiece(3);
+  board.dropPiece(2);
+  board.dropPiece(1);
+  board.dropPiece(2);
+  board.dropPiece(3);
+  board.dropPiece(3);
+	ASSERT_EQ(board.gameState(), X);
+}
+
+TEST(PiezasTest, oWin)
+{
+  Piezas board;
+  board.dropPiece(0);
+  board.dropPiece(0);
+  board.dropPiece(1);
+  board.dropPiece(1);
+  board.dropPiece(2);
+  board.dropPiece(2);
+  board.dropPiece(0);
+  board.dropPiece(3);
+  board.dropPiece(1);
+  board.dropPiece(3);
+  board.dropPiece(2);
+  board.dropPiece(3);
+	ASSERT_EQ(board.gameState(), O);
+}
