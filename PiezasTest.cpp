1e9d2bee4db48993227ebea032f7af4c57f65a1d
DeWubs <rwprouty55@gmail.com>
2019-12-13 16:42:33 -0800

    tests for peice at have been added

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -46,3 +46,29 @@ TEST(PiezasTest, dropPieceReturnBlank)
   board.dropPiece(0);
 	ASSERT_EQ(board.dropPiece(0), Blank);
 }
+
+/*Tests for pieceAt*/
+TEST(PiezasTest, noPiece)
+{
+  Piezas board;
+	ASSERT_EQ(board.pieceAt(0,2), Blank);
+}
+
+TEST(PiezasTest, onePiece)
+{
+  Piezas board;
+  board.dropPiece(0);
+	ASSERT_EQ(board.pieceAt(0,2), X);
+}
+
+TEST(PiezasTest, outOfBoundsrow)
+{
+  Piezas board;
+	ASSERT_EQ(board.pieceAt(4,0), Invalid);
+}
+
+TEST(PiezasTest, outOfBoundsCol)
+{
+  Piezas board;
+	ASSERT_EQ(board.pieceAt(0,4), Invalid);
+}
